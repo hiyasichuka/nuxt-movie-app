afe557e98404fceca6f4ac23e5bd28c99790ed67
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-03-17 23:42:41 +0900

    setup/sccs

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,8 +15,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
-
+  css: [
+    // プロジェクト内の SCSS ファイル
+    '@/assets/defaults.scss',
+  ],
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
