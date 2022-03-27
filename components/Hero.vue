342eabc6abdf464d483592b8592107a8122a9973
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-03-27 10:33:34 +0900

    moveDir

---
+++ components/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hero">
+    <img src="../assets/imgs/movieHero.jpeg" alt="" />
+    <div class="text-container">
+      <div class="text">
+        <span class="mini-heading">Now Streaming</span>
+        <h1><span>Now</span>Streaming</h1>
+        <a href="#movie-grid" class="button">View Movies</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+}
+</script>
+
+<style lang="sss" scoped>
+</style>
